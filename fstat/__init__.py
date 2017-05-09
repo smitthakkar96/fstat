99dc8036cab100a3bb0a49c34789efb5c4e5decd
smit thakkar <smitthakkar96@gmail.com>
2017-05-09 17:33:46 +0530

    sorted imports to fix pep8 warning

--- fstat/__init__.py
+++ fstat/__init__.py
@@ -3,12 +3,12 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+from controller import index
+from model import Failure, FailureInstance
+
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('default.cfg')
 app.config.from_pyfile('application.cfg', silent=True)
 Bootstrap(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-
-from controller import index
-from model import Failure, FailureInstance
