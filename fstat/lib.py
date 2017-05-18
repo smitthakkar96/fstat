653cbc3e8ead1b285849206e8c809d716ea05d5f
smit thakkar <smitthakkar96@gmail.com>
2017-05-18 10:29:16 +0530

    Removed x_days_ago

--- fstat/lib.py
+++ fstat/lib.py
@@ -1,14 +1,6 @@
 from datetime import datetime, timedelta
 
 
-def x_days_ago(x=1):
-    '''
-    Return timestamp for X days ago
-    '''
-    return (datetime.today().replace(hour=0, minute=0, second=0,
-            microsecond=0) - timedelta(days=x))
-
-
 def parse_start_date(start_date=None):
     if not start_date:
         today = datetime.today().replace(hour=0, minute=0, second=0,
