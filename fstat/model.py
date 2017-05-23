a439e9c1973533f02f3dfc77d48d4bf75c9a6e9c
smit thakkar <smitthakkar96@gmail.com>
2017-05-24 00:47:14 +0530

    made the requested changes

--- fstat/model.py
+++ fstat/model.py
@@ -12,7 +12,7 @@ STATE = (
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    github_username = db.Column(db.Integer, unique=True)
+    username = db.Column(db.Integer, unique=True)
     email = db.Column(db.String, unique=True)
     profile_picture = db.Column(db.String)
     token = db.Column(db.String)
