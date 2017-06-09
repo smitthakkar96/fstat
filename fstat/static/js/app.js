06536290b50bd048d64feee4a3aa4ef82af1ff12
smit thakkar <smitthakkar96@gmail.com>
2017-06-09 19:09:18 +0530

    Added global listener namespace to have all global javascript functions

--- fstat/static/js/app.js
+++ fstat/static/js/app.js
@@ -8,13 +8,6 @@
         window.location = location + `?start_date=${start}&end_date=${end}`;
     });
 
-    function showModal(fid, bugs) {
-        failure_id = fid
-        $('#associate-bug-modal').modal('show');
-        $('#bugIds').val(bugs);
-        $('#bugIds').tagsinput();
-    }
-
     function hasNaN(array) {
         for (let i = 0; i < array.length; i++) {
             if (isNaN(array[i])) {
@@ -23,31 +16,38 @@
         }
         return false;
     }
-
-    function submit() {
-        if ($('#bugIds').val() !== "") {
-            var bugIds = $('#bugIds').val().split(',').map(function(bugId){return parseInt(bugId)});
-                if (!hasNaN(bugIds)) {
-                    var URL = `/associate-bugs/${failure_id}`;
-                    $.ajax({
-                        url: URL,
-                        beforeSend: function(xhr){
-                            xhr.setRequestHeader("Content-Type","application/json");
-                        },
-                        data: JSON.stringify({bugIds: bugIds}),
-                        type: 'POST',
-                        success: function(data, status, xhr){
-                            location.reload();
-                        },
-                        error: function(err, status, xhr){
-                            alert(err.response);
-                        }
-                    });
-                } else {
-                    alert("All BugIDs must be integer.")
-                }
-        } else {
-            alert("Please enter at least one bug number.")
+    listeners = {
+        showModal: function(fid, bugs) {
+            failure_id = fid
+            $('#associate-bug-modal').modal('show');
+            $('#bugIds').val(bugs);
+            $('#bugIds').tagsinput();
+        },
+        submit: function() {
+            if ($('#bugIds').val() !== "") {
+                var bugIds = $('#bugIds').val().split(',').map(function(bugId){return parseInt(bugId)});
+                    if (!hasNaN(bugIds)) {
+                        var URL = `/associate-bugs/${failure_id}`;
+                        $.ajax({
+                            url: URL,
+                            beforeSend: function(xhr){
+                                xhr.setRequestHeader("Content-Type","application/json");
+                            },
+                            data: JSON.stringify({bugIds: bugIds}),
+                            type: 'POST',
+                            success: function(data, status, xhr){
+                                location.reload();
+                            },
+                            error: function(err, status, xhr){
+                                alert(err.response);
+                            }
+                        });
+                    } else {
+                        alert("All BugIDs must be integer.")
+                    }
+            } else {
+                alert("Please enter at least one bug number.")
+            }
         }
-    }
+    };
 }());
